7f1860661c2488e49432ac12702756bf14603a15
Rwabutara Kasami Ivan <kasami8000@gmail.com>
2026-08-28 23:08:20 +0300

    Update

--- Data Calling.R
+++ Data Calling.R
@@ -1,36 +1,29 @@
 
 ######################CALLING DATA ONE VARIABLE#######################################################3
-resp <- req |>
-  req_url_path_append("analytics.json") |>
+dx_id <- "sv6SeKroHPV"   # just one UID
+
+resp <- req %>%
+  req_url_path_append("analytics.json") %>%
   req_url_query(
-    dimension = c("dx:tgkH6h8gsWk", "pe:202512", "ou:iTfbtn6JUsN"),
+    dimension = c(
+      paste0("dx:", dx_id),
+      "pe:2022;2023;2024;2025;2026;2027",
+      "ou:ARrKg065SFL"
+    ),
     .multi = "explode"
-  ) |>
+  ) %>%
   req_perform()
 
 data <- resp_body_json(resp, simplifyVector = TRUE)
-
 df <- as.data.frame(data$rows, stringsAsFactors = FALSE)
-colnames(df) <- data$headers$column
+names(df) <- data$headers$column
 
-# metaData$items is a named list: UID -> list(name = "...")
-items <- data$metaData$items
-
-# helper to look up the readable name for a UID
-uid_to_name <- function(uid) {
-  vapply(uid, function(x) {
-    if (!is.null(items[[x]]) && !is.null(items[[x]]$name)) {
-      items[[x]]$name
-    } else {
-      x  # fallback to the UID if no name found
-    }
-  }, character(1))
-}
-
-df$Data <- uid_to_name(df$Data)
-df$`Organisation unit` <- uid_to_name(df$`Organisation unit`)
-
+item_names <- vapply(data$metaData$items, `[[`, character(1), "name")
+lookup <- function(uid) ifelse(uid %in% names(item_names), item_names[uid], uid)
+df$Data <- lookup(df$Data)
+df$`Organisation unit` <- lookup(df$`Organisation unit`)
 df
+
 
 ################# List all the indicator UIDs you want here############################
 dx_ids <- c("tgkH6h8gsWk", "DuMMAbvDfjn", "hqGqG3XbnJm")
@@ -87,4 +80,3 @@ df_long
 
 
 #######################################################################################
-
